a54fc08216338bbfe781828b3f10a1c8b5a1bd44
brimwell <142336400+brimwell@users.noreply.github.com>
2025-05-21 15:40:08 -0600

    Add basic funtionality of Dom based rps game

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-const list = document.querySelector('#shoppinglist');
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
 
 
-btn.addEventListener('click', () => {
-    let item = input.value;
-    input.value = '';
-
-    const listItem = document.createElement('li');
-    const span = document.createElement('span');
-    const newButton = document.createElement('button');
-
-    listItem.appendChild(span);
-    listItem.appendChild(newButton);
-    span.textContent = item;
-    newButton.textContent = 'Delete';
-
-    list.appendChild(listItem);
-
-    newButton.addEventListener('click', () => {
-        listItem.remove();
-    });
-
-    input.focus();
-});
-
-
-// The Hidden Console Game of Rock, Paper, Scissors
+// The Game of Rock, Paper, Scissors
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 9);
@@ -42,55 +16,90 @@ function getComputerChoice() {
 
     return compChoice;
 }
-
-function getHumanChoice() {
-    let humChoice = (prompt('Choose rock, paper, or scissors')).toLowerCase();
+//  Don't need if I have buttons in DOM
+// function getHumanChoice() {
+//     let humChoice = (prompt('Choose rock, paper, or scissors')).toLowerCase();
     
-    return humChoice;
-}
+//     return humChoice;
+// }
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const resultsBox = document.querySelector('#resultsbox');
+const resultsText = document.querySelector('#resultstext');
+const humanScoreDisplay = document.querySelector('#humanscore');
+const computerScoreDisplay = document.querySelector('#computerscore');
+const declareWinner = document.createElement('p');
+
+rock.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+});
+paper.addEventListener('click', () => {
+    playRound('paper', getComputerChoice());
+});
+scissors.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+});
+
+let humanScore = 0;
+let computerScore = 0;
+
+humanScoreDisplay.textContent = humanScore;
+computerScoreDisplay.textContent = computerScore;
+
+
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock') {
         if (computerChoice === 'rock') {
-            console.log(`It's a tie! You both chose ${humanChoice}.`);
-            return 'tie';
+            resultsText.textContent = `It's a tie! You both chose ${humanChoice}.`;
         } else if (computerChoice === 'paper') {
-            console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
-            return 'computer';
+            resultsText.textContent = `You lose, ${computerChoice} beats ${humanChoice}.`;
+            computerScore++;
         } else if (computerChoice === 'scissors') {
-            console.log(`You win, ${humanChoice} beats ${computerChoice}.`);
-            return 'human';
+            resultsText.textContent = `You win, ${humanChoice} beats ${computerChoice}.`;
+            humanScore++;
         } else {
-            console.log(`Hmmm, there seems to be something wrong.`);
+            resultsText.textContent = `Hmmm, there seems to be something wrong.`;
         }
     } else if (humanChoice === 'paper') {
         if (computerChoice === 'rock') {
-            console.log(`You win, ${humanChoice} beats ${computerChoice}.`);
-            return 'human';
+            resultsText.textContent = `You win, ${humanChoice} beats ${computerChoice}.`;
+            humanScore++;
         } else if (computerChoice === 'paper') {
-            console.log(`It's a tie! You both chose ${humanChoice}.`);
-            return 'tie';
+            resultsText.textContent = `It's a tie! You both chose ${humanChoice}.`;
         } else if (computerChoice === 'scissors') {
-            console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
-            return 'computer';
+            resultsText.textContent = `You lose, ${computerChoice} beats ${humanChoice}.`;
+            computerScore++;
         } else {
-            console.log(`Hmmm, there seems to be something wrong.`);
+            resultsText.textContent = `Hmmm, there seems to be something wrong.`;
         }
     } else if (humanChoice === 'scissors') {
         if (computerChoice === 'rock') {
-            console.log(`You lose, ${computerChoice} beats ${humanChoice}.`);
-            return 'computer';
+            resultsText.textContent = `You lose, ${computerChoice} beats ${humanChoice}.`;
+            computerScore++;
         } else if (computerChoice === 'paper') {
-            console.log(`You win, ${humanChoice} beats ${computerChoice}.`);
-            return 'human';
+            resultsText.textContent = `You win, ${humanChoice} beats ${computerChoice}.`;
+            humanScore++;
         } else if (computerChoice === 'scissors') {
-            console.log(`It's a tie! You both chose ${humanChoice}.`);
-            return 'tie';
+            resultsText.textContent = `It's a tie! You both chose ${humanChoice}.`;
         } else {
-            console.log(`Hmmm, there seems to be something wrong.`);
+            resultsText.textContent = `Hmmm, there seems to be something wrong.`;
         }
     } else {
-        console.log(`Hmmm, there seems to be an issue with your choice.`);
+        resultsText.textContent = `Hmmm, there seems to be an issue with your choice.`;
+    }
+
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+
+    if (humanScore === 5) {
+        declareWinner.textContent = `You win the game with a score of ${humanScore} over the computer's score of ${computerScore}. Congratulations!`;
+        resultsBox.appendChild(declareWinner);
+    } else if (computerScore === 5) {
+         declareWinner.textContent = `The computer wins the game with a score of ${computerScore} over your score of ${humanScore}. Better luck next time!`;
+        resultsBox.appendChild(declareWinner);
     }
 }
 
